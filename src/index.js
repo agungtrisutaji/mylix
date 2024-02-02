a6ccf74c0e4574f555f784a40226627b6b3343c4
Agung Trisutaji <agung.trisutaji.aprian@gmail.com.com>
2024-02-02 15:19:41 +0700

    add rating component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import StarRating from './components/Rating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating />
+    <StarRating max={10} color='blue' size={40} />
   </React.StrictMode>
 );
